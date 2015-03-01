ec09fa2737563bf9949e59c82323b1c2e1c1126b
Mostafa Rohaninejad <rohaninejadm@berkely.edu>
2015-03-01 13:44:30 -0800

    boardstate and move

---
+++ src/Move.java
@@ -0,0 +1,37 @@
+public class Move{
+	public boolean isCapture;
+	public int xi;
+	public int yi;
+	public int xf;
+	public int yf;
+	public Move(int xi, int yi, int xf, int yf){
+		this.xi = xi;
+		this.yi = yi;
+		this.xf = xf;
+		this.yf = yf;
+		if(Math.abs(xi - xf) == 2){
+			isCapture = true;
+		}else{
+			isCapture = false;
+		}
+	}
+	public boolean isCapture(){
+		return isCapture;
+	}
+	public int xf(){
+		return xf;
+	}
+	public int yf(){
+		return yf;
+	}
+	public int xi(){
+		return xi;
+	}
+	public int yi(){
+		return yi;
+	}
+	/*
+	public void changeBoard(Board b){
+		return;
+	}*/
+}
