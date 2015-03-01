0db563bf59f4c51750c1f8b13e1f04de31190348
Michael Zhang <michaelzhang333@gmail.com>
2015-03-01 12:40:30 -0800

    Add gametree

---
+++ src/FindBestMove.java
@@ -0,0 +1,12 @@
+public class FindBestMove{
+    /**
+     * Things to consider (most important at top):
+     * learning from opponent
+     * evaluate value of position
+     * opening moves
+     * optimal end game playing (could search deeper in end game)
+     */
+
+    // Essentially want to choose the move with best score
+    GameTree state;
+}
