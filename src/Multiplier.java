2a55579684eba2c51471f9dbcec31b9c2936ba48
Michael Zhang <michaelzhang333@gmail.com>
2015-03-01 17:15:53 -0800

    simple evaluating board

---
+++ src/Multiplier.java
@@ -0,0 +1,18 @@
+/**
+ * Used as helper class for EvalBoard
+ */
+
+public class Multiplier{
+    int x;
+    int y;
+    int multiplier;
+
+    public Multilier(){
+    }
+
+    public Multilier(int x, int y, int multiplier){
+        this.x = x;
+        this.y = y;
+        this.multiplier = multiplier;
+    }
+}
