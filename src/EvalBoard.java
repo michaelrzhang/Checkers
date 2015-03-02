1dd7072510c595c661241fee8216a5c991363d2e
Michael Zhang <michaelzhang333@gmail.com>
2015-03-01 16:35:03 -0800

    remove class files

---
+++ src/EvalBoard.java
@@ -0,0 +1,14 @@
+public class EvalBoard{
+
+// 10-11-12-13-14
+// 00-01-02-03-04
+    public double evalBoardSimple(int[][] board){
+        double count;
+        int temp;
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+                temp = board[i][j];
+            }
+        }
+    }
+}
