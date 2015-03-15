ff92bf498d39fb666eb1dd1df8c7f4d164288368
Michael Zhang <michaelzhang333@gmail.com>
2015-03-15 01:44:14 -0700

    some questions

--- src/Ai.java
+++ src/Ai.java
@@ -1,6 +1,8 @@
 package src;
+
+/** Class that we should extend for our AI? */
 public abstract class Ai {
-	int player;
+	int player;  // is 1 red and -1 blue?
 	Board board;
 	public Ai (Board b, int player) {
 		this.board = b;
